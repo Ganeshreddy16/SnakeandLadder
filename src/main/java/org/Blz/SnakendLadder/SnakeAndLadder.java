dd966c508621c9e9617b449fe52de3c8c859cf73
Ganesh Reddy <reddyganesh125@gmail.com>
2021-08-26 22:52:20 +0530

    Added:Project Structure Position 0

--- src/main/java/org/Blz/SnakendLadder/SnakeAndLadder.java
+++ src/main/java/org/Blz/SnakendLadder/SnakeAndLadder.java
@@ -2,8 +2,11 @@ package org.Blz.SnakendLadder;
 
 public class SnakeAndLadder
 {
+    public static final int TOTAL_NUMBERS=100;
     public static void main( String[] args )
     {
         System.out.println( "Welcome to The Snake and Ladder Game" );
+       int StartingPosition=0;
+       System.out.println("Your Number Position is "+ StartingPosition);
     }
 }
