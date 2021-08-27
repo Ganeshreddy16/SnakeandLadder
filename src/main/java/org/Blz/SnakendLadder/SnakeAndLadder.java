ba47ba0343d2865905d87ea8bbcafffd80cb8cff
Ganesh Reddy <reddyganesh125@gmail.com>
2021-08-27 13:46:39 +0530

    Added:Player Goes till 100 and Zero if position less than zero

--- src/main/java/org/Blz/SnakendLadder/SnakeAndLadder.java
+++ src/main/java/org/Blz/SnakendLadder/SnakeAndLadder.java
@@ -8,22 +8,28 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to The Snake and Ladder Game");
 
-        int DiceNum = (int) (Math.floor(Math.random() * 10) % 6 + 1);
-        System.out.println("Your Dice Roll Number is " + DiceNum);
+        while (CurrentPosition<=100) {
 
-        int NoPlay = 1;
-        int Ladder = 2;
-        int Snake = 3;
-        int Options = (int) (Math.floor(Math.random() * 10) % 3 + 1);
-        if (Options == NoPlay) {
-            CurrentPosition += 0;
-            System.out.println("You got No Play and Your current position is " + CurrentPosition);
-        } else if (Options == Ladder) {
-            CurrentPosition += DiceNum;
-            System.out.println("You got Ladder and Your current position is " + CurrentPosition);
-        } else if (Options == Snake) {
-            CurrentPosition -= DiceNum;
-            System.out.println("You were caught by snake and Your current position is " + CurrentPosition);
+            int DiceNum = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+            System.out.println("Your Dice Roll Number is " + DiceNum);
+
+            int NoPlay = 1;
+            int Ladder = 2;
+            int Snake = 3;
+            int Options = (int) (Math.floor(Math.random() * 10) % 3 + 1);
+            if (Options == NoPlay) {
+                CurrentPosition += 0;
+                System.out.println("You got No Play and Your current position is " + CurrentPosition);
+            } else if (Options == Ladder) {
+                CurrentPosition += DiceNum;
+                System.out.println("You got Ladder and Your current position is " + CurrentPosition);
+            } else if (Options == Snake) {
+                if (CurrentPosition <= 0) {
+                    CurrentPosition = 0;
+                }
+                CurrentPosition -= DiceNum;
+                System.out.println("You were caught by snake and Your current position is " + CurrentPosition);
+            }
         }
     }
 
