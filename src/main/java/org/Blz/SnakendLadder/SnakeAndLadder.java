c1c2386e746f88afc4fd3656e0e1fecf1515e38e
Ganesh Reddy <reddyganesh125@gmail.com>
2021-08-27 14:29:07 +0530

    Added:ReportTheNoOfTimesDiceRolled

--- src/main/java/org/Blz/SnakendLadder/SnakeAndLadder.java
+++ src/main/java/org/Blz/SnakendLadder/SnakeAndLadder.java
@@ -4,6 +4,7 @@ public class SnakeAndLadder {
     public static final int TOTAL_NUMBERS = 100;
     public static int InitialPosition = 0;
     public static int CurrentPosition = 0;
+    public static int DiceCount = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to The Snake and Ladder Game");
@@ -11,6 +12,7 @@ public class SnakeAndLadder {
         while (CurrentPosition < 100) {
 
             int DiceNum = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+            DiceCount += 1;
             System.out.println("Your Dice Roll Number is " + DiceNum);
 
             int NoPlay = 1;
@@ -21,19 +23,22 @@ public class SnakeAndLadder {
                 CurrentPosition += 0;
                 System.out.println("You got No Play and Your current position is " + CurrentPosition);
             } else if (Options == Ladder) {
-                if (CurrentPosition+ DiceNum<=100){
+                if (CurrentPosition + DiceNum <= 100) {
                     CurrentPosition += DiceNum;
                 }
 
                 System.out.println("You got Ladder and Your current position is " + CurrentPosition);
             } else if (Options == Snake) {
-                if (CurrentPosition <= 0) {
+                if (CurrentPosition <= 0)
+                {
                     CurrentPosition = 0;
                 }
                 CurrentPosition -= DiceNum;
                 System.out.println("You were caught by snake and Your current position is " + CurrentPosition);
             }
+
         }
+        System.out.println("No.of times dice  rolled till now "+ DiceCount);
     }
 
 }
